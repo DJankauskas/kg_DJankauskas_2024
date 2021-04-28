1b1082edab516dce16f2ecf2370761d4b2a7ed3f
Darius Jankauskas <d5926j@gmail.com>
2021-04-28 02:13:25 -0400

    App skeleton.

---
+++ main.js
@@ -0,0 +1,15 @@
+function intsToPronunciations(ints) {
+    // TODO: implement
+    return "";
+}
+
+function main() {
+    // gets the command line arguments, skipping "node" and the file path
+    const args = process.argv.slice(2);
+    try {
+        console.log(intsToPronunciations(args))
+    } catch(_e) {
+        // integer parsing or processing failed
+        console.error("Usage: node main.js [integer 1] [integer 2] ...")
+    }
+}
